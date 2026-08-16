e229d034c099c18944c952d3cc21e02cfb088164
etvjay <etvictory620@gmail.com>
2026-08-16 03:44:59 +0100

    db: qualify legacy vector index drop

--- db/migrations/006_agent_scoped_vector_index.sql
+++ db/migrations/006_agent_scoped_vector_index.sql
@@ -1,4 +1,4 @@
-DROP INDEX IF EXISTS memories_embedding_idx;
+DROP INDEX IF EXISTS memories@memories_embedding_idx;
 
 CREATE VECTOR INDEX IF NOT EXISTS memories_agent_embedding_idx
   ON memories (agent_id, embedding);
