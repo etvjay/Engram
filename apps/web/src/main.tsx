a02f7674835638468c76fc82c198f9902d4ad40c
etvjay <etvictory620@gmail.com>
2026-08-21 06:03:14 +0100

    frontend: route designed landing and canonical proof separately

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./app/App";
-import "./styles.css";
-import "./research.css";
 
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const env = (import.meta as ImportMeta & { env: Record<string, string> }).env;
+const base = (env.BASE_URL ?? "/").replace(/\/$/, "");
+const isProofRoute = window.location.pathname === `${base}/proof` || window.location.hash === "#proof";
+const root = createRoot(document.getElementById("root")!);
+
+if (isProofRoute) {
+  void import("./app/App").then(({ App }) => {
+    root.render(<React.StrictMode><App /></React.StrictMode>);
+  });
+} else {
+  void import("./landing").then(({ Landing }) => {
+    root.render(<React.StrictMode><Landing /></React.StrictMode>);
+  });
+}
